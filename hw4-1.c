e181867ce7a7d8bb701f39d8e4cb1329752fe373
bowjason <146065148+bowjason@users.noreply.github.com>
2023-10-26 18:44:22 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+int main()
+{
+int x,y,z,a=0;
+scanf("%d",&z);
+int b[z];
+while(a<z){
+scanf("%d",&b[z]);
+for(int i=2;i<b[z];i++){
+	y=b[z]%i;
+	if(y==0){
+		printf("NO"); printf(" ");
+		i=b[z];
+	}
+	if(i==b[z]-1){
+		printf("YES");printf(" ");
+	}
+}
+if(b[z]==2){
+		printf("YES");printf(" ");
+		
+	}
+	a=a+1;
+	}
+}
